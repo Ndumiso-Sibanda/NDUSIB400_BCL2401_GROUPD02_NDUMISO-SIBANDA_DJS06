e523c47f64bf9ba56d4a4f6f90797d093875a653
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-05-22 12:55:42 +0200

    Add example to calculate name lengths using map method

--- scripts.js
+++ scripts.js
@@ -38,3 +38,8 @@ names.forEach((name, index) => {
 console.log("\n2. Uppercase Transformation:");
 const uppercaseProvinces = provinces.map((province) => province.toUpperCase());
 console.log(uppercaseProvinces);
+
+//3. Name Lengths
+console.log("\n3. Name Lengths:");
+const nameLenths = names.map((name) => name.length);
+console.log(nameLenths);
